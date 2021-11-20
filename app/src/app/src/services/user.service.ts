148b74dac6d52105ad518f9bb985f283aa0cacbf
SirADV24 <vranciuadrian24@gmail.com>
2021-11-20 15:05:04 +0200

    login servie

--- app/src/app/src/services/user.service.ts
+++ app/src/app/src/services/user.service.ts
@@ -6,22 +6,14 @@ import { LoginRequest } from '../model/user/login.request';
 
 @Injectable()
 export class UserService {
-  private apiURL = '';
+  private apiURL = 'http://localhost:8080/login';
 
   constructor(private httpClient: HttpClient) {}
 
   login(loginRequest: LoginRequest): Observable<LoginResponse> {
-    // Use this once API functionality is implemented
-
-    // return this.httpClient.post<LoginResponse>(
-    //   `${this.apiURL}/login`,
-    //   loginRequest
-    // );
-
-    return of({
-      userId: 1,
-      username: 'Gioni',
-      mail: 'gioni@mail.com',
-    });
+    return this.httpClient.post<LoginResponse>(
+      `${this.apiURL}/login`,
+      loginRequest
+    );
   }
 }
